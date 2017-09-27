fc52a154130a942794c6db27533869c244a3301b
lawenliu <lwc541117@gmail.com>
2017-09-26 19:19:47 -0500

    Angular Technology

---
+++ InterviewQuestions/Angular/TemplateSyntax/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'my-app',
+	template: `
+		<h1>App Header</h1>
+		<my-heroes></my-heroes>
+	`
+})
+
+export class AppComponent {}
